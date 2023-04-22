19d9269f2973886e6e48c743fea97603a6765276
jomanaaljarallah <jarallahjomana@gmal.com>
2023-04-22 15:37:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1=2;
+const num2=31;
+let multiply = num1*num2;
+let random = Math.ceil(Math.random() * 10);
+const num3 = 14;
+const num4 = 5;
+
+const mod = num3 % num4;
+const numbers=[12,15,20,17];
+const max = Math.max(...numbers);
